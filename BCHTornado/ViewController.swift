5d571642ffc4131a62757f57bae315dcb0d62ceb
Xiaofeng <ylsongfeng@gmail.com>
2018-07-22 06:29:46 +0800

    add test code of send raw transaction to main net

--- BCHTornado/ViewController.swift
+++ BCHTornado/ViewController.swift
@@ -20,26 +20,38 @@ class ViewController: UIViewController {
         let wallet = Wallet(mnemonic: words)
         NSLog("address: %@", wallet.mainAddress)
         
-//        let outputArray = BitboxApi().unspentOutputs(withAddress: "17Qp9DRhgZvEqM9waX88QkGU9g86ABY5uc") as? [BTCTransactionOutput]
         
-        var errorOut: Error?
-        var unwrappedUtxos: [BTCTransactionOutput]?
         
-        do {
-            unwrappedUtxos = try BitboxApi().unspentOutputs(withAddress: "17Qp9DRhgZvEqM9waX88QkGU9g86ABY5uc") as? [BTCTransactionOutput]
-        } catch {
-            errorOut = error
-        }
-        print("UTXOs for 17Qp9DRhgZvEqM9waX88QkGU9g86ABY5uc: \(String(describing: unwrappedUtxos)) \(String(describing: errorOut))")
-        
+        // 这两个都是我的bitpie地址
         let receiver = ReceiveInfo(address: "1GJZp8GP2i9KBkiaqr1chT1j9XPGPvHf9D", amount: BigInt(100000))
+        let receiver2 = ReceiveInfo(address: "1PsvF5hgepvparfSSZTN42ZAq2ne6L9ife", amount: BigInt(100000))
         do {
-            let tx = try wallet.sign(toValues: [receiver])
+            let tx = try wallet.sign(toValues: [receiver, receiver2])
             let rawTxData = tx.data.toHexString()
             print(rawTxData)
+            
+            let request = BitboxApi().requestForTransactionBroadcast(with: tx.data)
+            
+            let task = URLSession.shared.dataTask(with: request as! URLRequest) { (data, response, error) in
+                if error != nil{
+                    print(error.debugDescription)
+                }else{
+                    let str = String(data: data!, encoding: String.Encoding.utf8)
+                    print(str)
+                }
+            } as URLSessionTask
+            
+            // 测试发送可以打开注释
+//            task.resume()
+        
+            
+            
+            
         } catch {
             
         }
+        
+        
         
     }
 
